cd6725efe19a97fcce85841e516015412f831d91
jcontour <contj186@newschool.edu>
2015-11-21 17:42:13 -0500

    render

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@ var app = app || {};
 
 app.main = (function() {
 
+	var render = function(template, containerElement, method, data){
+		console.log(method + ' ' + template + ' in ' + containerElement);
+		if(data !== undefined){
+			console.log(data);
+		}
+
+		var templateToCompile = $('#tpl-' + template).html();
+		var compiled =  _.template(templateToCompile);
+
+		if(method === 'replace'){
+			$(containerElement).html(compiled({data: data}));	
+		}else if(method === 'append'){
+			$(containerElement).append(compiled({data: data}));
+		}
+
+
 	var attachEvents = function(){
 		//listening for clicks. 
 	}
 
 	var init = function(){
 		attachEvents();
+		render();
 	};
 
 	return {
