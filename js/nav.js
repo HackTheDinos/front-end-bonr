0962f9877a8d38d234ae360b3348656b5f96e83a
sabotai <sabotaione@gmail.com>
2015-11-22 06:34:01 -0500

    dropdown integration

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,8 @@
-document.querySelector( "#nav-toggle" )
-  .addEventListener( "click", function() {
+document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
     this.classList.toggle( "active" );
   });
+
+  $("#nav-toggle").click(function togglemenu() {
+    console.log("running");
+    $('.dropdownmenu').toggleClass("menudropped");
+});
